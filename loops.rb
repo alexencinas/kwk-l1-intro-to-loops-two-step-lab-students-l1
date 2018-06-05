0bd20f0e226da378ca71794929122641ae052551
alexencinas <shoogiee123@gmail.com>
2018-06-05 20:50:36 +0000

    automatically backed up by learn

--- loops.rb
+++ loops.rb
@@ -28,3 +28,16 @@ end
 
 dance
 
+
+
+
+
+def dance 
+  moves_completed = 0
+  
+  while moves_completed < 10
+  puts "to the left, to the left"
+  puts "walk it out"
+  
+  moves_completed +=1
+  #moves_completed = moves_completed +1
